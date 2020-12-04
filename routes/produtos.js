bb77dda75a4603a390658b973b4b034848ce26cf
tarcisio.philips <tarcisio.philips@unifesp.br>
2020-12-04 15:15:24 -0300

    feat: routes 'pedidos' and 'produtos'

---
+++ routes/produtos.js
@@ -0,0 +1,48 @@
+const express = require('express');
+const router = express.Router();
+
+//RETORNA TODOS OS PRODUTOS
+router.get('/', (req, res, next) => {
+  res.status(200).send({
+    mensagem:"Retorna todos os pedidos"
+  });
+});
+//INSERE UM PRODUTO
+router.post('/', (req, res, next) => {
+  res.status(201).send({
+    mensagem: "Insere um produto"
+  });
+});
+
+//RETORNA OS DADOS DE UM PRODUTO
+router.get('/:id_produto', (req, res, next) =>{
+  const id = req.params.id_produto
+
+  if( id === 'especial'){
+    res.status(200).send({
+      mensagem: 'Voce descobriu o id especial' ,
+      id: id
+    });
+  } else {
+    res.status(200).send({
+      mensagem:'Você passou um ID',
+      id: id
+    });
+  }
+});
+
+//ALTERA UM PRODUTO
+router.patch('/', (req, res, next) => {
+  res.status(201).send({
+    mensagem:"Produto alterado"
+  });
+});
+
+//EXCLUI UM PRODUTO
+router.delete('/', (req, res, next) => {
+  res.status(201).send({
+    mensagem:"Produto deletado"
+  });
+});
+
+module.exports = router
